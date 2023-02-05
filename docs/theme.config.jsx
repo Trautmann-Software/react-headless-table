1e559f7d893034d956f1caa031d261fae2713edb
Rashad Asgarbayli <rashad2985@gmail.com>
2023-02-05 12:58:45 +0100

    docs: added npm link

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -18,16 +18,16 @@ export default {
       </svg>
     )*/
   },
-  /*
   chat: {
-    link: 'https://discord.gg/5sjTep9yvC'
+    link: 'https://www.npmjs.com/package/@trautmann/react-headless-table',
     // If icon is missing, it will be a Discord icon by default.
-    icon: <svg width="24" height="24" viewBox="0 0 256 256">
-      <path fill="currentColor"
-            d="m231.9 169.8l-94.8 65.6a15.7 15.7 0 0 1-18.2 0l-94.8-65.6a16.1 16.1 0 0 1-6.4-17.3L45 50a12 12 0 0 1 22.9-1.1L88.5 104h79l20.6-55.1A12 12 0 0 1 211 50l27.3 102.5a16.1 16.1 0 0 1-6.4 17.3Z"></path>
-    </svg>
+    icon: (
+      <svg width="24" height="24" viewBox="0 0 27.23 27.23">
+        <rect fill="#333333" width="27.23" height="27.23" rx="2"></rect>
+        <polygon fill="#fff" points="5.8 21.75 13.66 21.75 13.67 9.98 17.59 9.98 17.58 21.76 21.51 21.76 21.52 6.06 5.82 6.04 5.8 21.75"></polygon>
+      </svg>
+    )
   },
-  */
   /*banner: {
     dismissible: true,
     key: '1.0.0-release',
